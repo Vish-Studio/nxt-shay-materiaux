e6cbf76bce91e95dc412141188ec25ce805c5ca5
VishSeen <vishseenarain@gmail.com>
2024-10-14 07:20:22 +0400

    [DEV] - top-bar now fixed. profile size decrease for cleaner ui.

--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -13,8 +13,8 @@ const Profile: FunctionComponent<ProfileProps> = ({
       <Image
         src={imgUrl}
         alt={name}
-        width={50}
-        height={50} />
+        width={40}
+        height={40} />
     </div>
   )
 }
