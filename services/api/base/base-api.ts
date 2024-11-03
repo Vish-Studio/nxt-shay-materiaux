750a58d8c14252ba847cc5467f1f7477a13265b2
Divesh Heeramun <divesh.heeramun1@gmail.com>
2024-11-03 10:32:11 +0400

    Add missing service wrappers (#29)

--- services/api/base/base-api.ts
+++ services/api/base/base-api.ts
@@ -1,6 +1,17 @@
 import axios from 'axios';
 
-class ApiService {
+type TBody = {
+  [key: string]: string;
+};
+
+interface IApiService {
+  post(endpoint: string, body: TBody): Promise<any>;
+  get(endpoint: string): Promise<any>;
+  put(endpoint: string, body: TBody): Promise<any>;
+  delete(endpoint: string, body?: TBody): Promise<any>;
+}
+
+class ApiService implements IApiService {
   public baseUrl: string;
   private envBaseUrl: string = process.env.NEXT_PUBLIC_API_BASE_URL as string;
 
@@ -8,11 +19,53 @@ class ApiService {
     this.baseUrl = this.envBaseUrl;
   }
 
-  public async post(endpoint: string, body: { [key: string]: string }) {
+  public async post(endpoint: string, body: TBody): Promise<any> {
     const url = `${this.baseUrl}${endpoint}`;
 
     try {
       const response = await axios.post(url, body);
+      return {
+        data: response.data,
+        status: response.status
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async get(endpoint: string): Promise<any> {
+    const url = `${this.baseUrl}${endpoint}`;
+
+    try {
+      const response = await axios.get(url);
+      return {
+        data: response.data,
+        status: response.status
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async put(endpoint: string, body: TBody): Promise<any> {
+    const url = `${this.baseUrl}${endpoint}`;
+
+    try {
+      const response = await axios.put(url, body);
+      return {
+        data: response.data,
+        status: response.status
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async delete(endpoint: string, body?: TBody): Promise<any> {
+    const url = `${this.baseUrl}${endpoint}`;
+
+    try {
+      const response = await axios.delete(url, { data: body });
       return {
         data: response.data,
         status: response.status
