db1c4b98184747ac35de5288b501d9f571ab0c2f
VishSeen <vishseenarain@gmail.com>
2025-01-04 17:47:26 +0400

    [dev] - added table ui.

--- components/table/table-list/table-list.tsx
+++ components/table/table-list/table-list.tsx
@@ -18,42 +18,44 @@ const TableList: FunctionComponent<TableProps> = ({
   return (
     <table className="table-list">
       <tbody>
-        <tr onClick={click}>
-          <td className="status">
-            <TagPayment status="pending" />
-          </td>
-          <td className="name">
-            <p>John Dwayne Jr</p>
-            <span>Pamplemousses</span>
-          </td>
-          <td className="phone">
-            <p>Phone</p>
-            <span>+230 5123 4567</span>
-          </td>
-          <td className="date">
-            <p>Date</p>
-            <span>Monday</span>
-          </td>
-        </tr>
-
-
-        <tr>
-          <td className="status">
-            <TagPayment status="paid" />
-          </td>
-          <td className="name">
-            <p>John Dwayne Jr</p>
-            <span>Pamplemousses</span>
-          </td>
-          <td className="phone">
-            <p>Phone</p>
-            <span>+230 5123 4567</span>
-          </td>
-          <td className="date">
-            <p>Date</p>
-            <span>Monday</span>
-          </td>
-        </tr>
+        {
+          isClient ? (
+            <tr onClick={click}>
+              <td className="status">
+                <TagPayment status="pending" />
+              </td>
+              <td className="name">
+                <p>John Dwayne Jr</p>
+                <span>Pamplemousses</span>
+              </td>
+              <td className="phone">
+                <p>Phone</p>
+                <span>+230 5123 4567</span>
+              </td>
+              <td className="date">
+                <p>Date</p>
+                <span>Monday</span>
+              </td>
+            </tr>
+          ) : (
+            <tr>
+              <td className="status">
+                <TagPayment status="paid" />
+              </td>
+              <td className="name">
+                <p>John Dwayne Jr</p>
+                <span>Pamplemousses</span>
+              </td>
+              <td className="phone">
+                <p>Phone</p>
+                <span>+230 5123 4567</span>
+              </td>
+              <td className="date">
+                <p>Date</p>
+                <span>Monday</span>
+              </td>
+            </tr>
+          )}
       </tbody>
     </table>
   )
