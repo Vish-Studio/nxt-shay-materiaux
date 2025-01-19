b7e67b33cb7adb0b32db4f03bcfe0ef64801d82f
VishSeen <vishseenarain@gmail.com>
2025-01-20 03:25:22 +0400

    [dev] - added new fields for clients.

--- app/(dashboard)/clients/new/page.tsx
+++ app/(dashboard)/clients/new/page.tsx
@@ -40,31 +40,108 @@ export default function NewClients() {
               type="text"
               hint="First Name"
             />
-            {errors.email && <span>This field is required</span>}
+            {errors.firstName && <span>This field is required</span>}
+
             <FormInput
               {...register('lastName', { required: true, })}
               title="lastName"
               type="text"
               hint="Last Name"
             />
-            {errors.password && <span>This field is required</span>}
+            {errors.lastName && <span>This field is required</span>}
+
+            <FormInput
+              {...register('nid', { required: true, })}
+              title="nid"
+              type="text"
+              hint="National ID"
+            />
+            {errors.nid && <span>This field is required</span>}
           </div>
-          <br />
-          <FormInput
-            {...register('brn', { required: false, })}
-            title="brn"
-            type="text"
-            hint="Business Registration Number (BRN)"
-          />
-          {errors.password && <span>This field is required</span>}
-          <br />
-          <FormInput
-            {...register('shop', { required: true, })}
-            title="shop"
-            type="text"
-            hint="Shop Name"
-          />
-          {errors.password && <span>This field is required</span>}
+
+          <div className="contact-info vertical-fields">
+            <label htmlFor="tel">Contact</label>
+
+            <div className="horizontal-fields">
+              <FormInput
+                {...register('phone', { required: false })}
+                title="phone"
+                type="number"
+                hint="Phone"
+              />
+
+              <FormInput
+                {...register('mobile', { required: false })}
+                title="mobile"
+                type="number"
+                hint="Mobile"
+              />
+            </div>
+          </div>
+
+          <div className="address-info vertical-fields">
+            <div className="header">
+              <label htmlFor="adress">Address</label>
+              <span>Fill in required address or add from the map below.</span>
+            </div>
+
+            <FormInput
+              {...register('adress', { required: false })}
+              title="adress"
+              type="text"
+              hint="Addresse"
+            />
+
+            <FormInput
+              {...register('city', { required: false })}
+              title="city"
+              type="text"
+              hint="City"
+            />
+          </div>
+
+
+          <div className="business-info vertical-fields">
+            <div className="header">
+              <label htmlFor="shopName">Company</label>
+              <span>Details about the company of the client.</span>
+            </div>
+
+            <FormInput
+              {...register('shopName', { required: false, })}
+              title="shopName"
+              type="text"
+              hint="Shop name"
+            />
+
+            <FormInput
+              {...register('brn', { required: false })}
+              title="brn"
+              type="text"
+              hint="BRN"
+            />
+          </div>
+
+          <div className="delivery-info vertical-fields">
+            <div className="header">
+              <label htmlFor="shopName">Delivery</label>
+              <span>Add delivery date for reminders.</span>
+            </div>
+
+            <FormInput
+              {...register('date', { required: false, })}
+              title="date"
+              type="text"
+              hint="Delivery Date"
+            />
+
+            <FormInput
+              {...register('brn', { required: false })}
+              title="brn"
+              type="text"
+              hint="BRN"
+            />
+          </div>
         </form>
       </div>
 
